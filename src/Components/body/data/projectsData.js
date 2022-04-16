bb19d84de6c8b6877a75ebd12d042b70f1a7f814
AhmedKhaled13281 <ahmedrashad13281@yahoo.com>
2022-04-16 07:17:06 +0200

    updated

--- src/Components/body/data/projectsData.js
+++ src/Components/body/data/projectsData.js
@@ -1,7 +1,24 @@
 export const ProjectData = [
-
   {
-    id: 1,
+    catigory : 'react',
+    title : 'Netflix Clone',
+    about : "it's a Netflix Clone which is let you see the Traillers for your favorite movies and see their description and Rating",
+    tags : ["React" , "Styled-Component" , "React-Router" , "React-Hooks" , "Custom Hook" , "Css-Modules" , "Sass" ,"Responsive Design" , "Axios"],
+    demo : 'https://netflix-clone-ten-iota.vercel.app/',
+    github : 'https://github.com/AhmedKhaled13281/Netflix-Clone',
+    images : '../../assets/netflix.png'
+  },
+  {
+    catigory : 'react',
+    title : 'Shopping Cart',
+    about : "it's a Shopping Cart project which is let you buy a lot of categories of products and collect them in the cart , and easily to add or remove the product",
+    tags : ["React" , "Redux" , "React-Router" , "React-Hooks" , "Custom Hook" , "React-Bootstrap" , "Responsive Design" , "Axios"],
+    demo : 'https://shopping-cart-eight-silk.vercel.app/',
+    github : 'https://github.com/AhmedKhaled13281/Shopping-Cart',
+    images : '../../assets/shopping-cart.png'
+  },
+  {
+    catigory : 'react',
     title : 'Video Player',
     about : "it's a simple clone of Youtube that's let you watching an amazing videos and see the recommended for your search",
     tags : ["React" , "Hooks" , "Semantic Ui" , "Responsive Design" ] ,
@@ -10,8 +27,8 @@ export const ProjectData = [
     images : '../../assets/videoplayer.png'
   },
   {
-    id: 2,
-    title : 'Movies App',
+    catigory : 'vanilla js',
+    title : 'Movie Search',
     about : "it's a Movies App that's makes you see your best movies easily and see the ratings and the description of each movie",
     tags : ["Javascript" , "API" , "Html5" , "Css3" , "Responsive Design"],
     demo : 'https://movies-app-sable-sigma.vercel.app/',
@@ -19,7 +36,7 @@ export const ProjectData = [
     images : '../../assets/movies.png'
   },
   {
-    id: 3,
+    catigory : 'vanilla js',
     title : 'Bankist',
     about : "This is a landing page for online banking made use HTML, CSS and Vanilla javascript advanced DOM manipulation. some features like:- Lazy Loading Images, Dynamic Cards, and Testimonial Slider.Stack",
     tags : ["Javascript" , "Html5" , "Css3" , "Responsive Design"],
@@ -28,7 +45,7 @@ export const ProjectData = [
     images : '../../assets/bankist 1.png'
   },
   {
-    id: 4,
+    catigory : 'vanilla js',
     title : 'Weather App',
     about : "it's a Weather App that makes you see the weather in your city with geolocation and you can see any city by searching with it's name ",
     tags : ["Javascript" , "API" , "Html5" , "Css3" , "Responsive Design"],
@@ -38,6 +55,7 @@ export const ProjectData = [
   },
   {
     id: 5,
+    catigory : 'vanilla js',
     title : 'Rock Paper Scissor',
     about : "Rock paper scissors is a classic game with a very simple rule set which makes it perfect for a beginner game project",
     tags : ["Javascript" , "Html5" , "Css3" , "Responsive Design"],
@@ -46,7 +64,7 @@ export const ProjectData = [
     images : '../../assets/rock.png'
   },
   {
-    id: 6,
+    catigory : 'animation',
     title : 'Restaurant',
     about : "it's a Restaurant website with a beautiful animation and responsive design",
     tags : ["Javascript" , "Html5" , "Css3" , "Responsive Design" , "Animation"],
--- src/Components/body/projects/index.js
+++ src/Components/body/projects/index.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React , {useState} from 'react'
 import './projects.css'
 import {ProjectData} from '../data/projectsData'
 import ProjectCard from './project-card'
 import Separator from '../common/separator/index'
-
+import './projects.css'
 const Projects = () => {
+  const [data , setData] = useState(ProjectData)
+
   const renderProjects = () => {
-    return ProjectData.map(project => {
+    return data.map(project => {
       return (
         <div key={project.id}>
              <ProjectCard project={project} />
@@ -14,10 +16,22 @@ const Projects = () => {
       )
     })
   }
+
+  const filterProjects = (term) => {
+    const updateList = ProjectData.filter(x => x.catigory === term)
+    setData(updateList)
+  }
+
   return(
     <div className="project">
       <Separator />
         <label className="section-title">Projects</label>
+        <div className="buttons">
+          <button className="filter-button" onClick={() => setData(ProjectData)}>All</button>
+          <button className="filter-button" onClick={() => filterProjects('react')}>React</button>
+          <button className="filter-button" onClick={() => filterProjects('vanilla js')}>Vanilla Js</button>
+          <button className="filter-button" onClick={() => filterProjects('animation')}>Animation</button>
+      </div>
         <div>{renderProjects()}</div>
     </div>
   )
